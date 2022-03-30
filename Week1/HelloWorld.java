8ac2d3f2a79ddd776c9905ec84b1a074f935a9ee
a88126451 <49521685+a88126451@users.noreply.github.com>
2022-03-30 13:56:29 +0800

    Week 1

--- Week1/HelloWorld.java
+++ Week1/HelloWorld.java
@@ -13,8 +13,8 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 /** HelloWorld
   * An application with two maps side-by-side zoomed in on different locations.
   * Author: UC San Diego Coursera Intermediate Programming team
-  * @author Your name here
-  * Date: July 17, 2015
+  * @author Joyce Lin
+  * Date: Mar 22, 2022
   * */
 public class HelloWorld extends PApplet
 {
